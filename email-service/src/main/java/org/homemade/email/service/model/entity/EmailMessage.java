f1d00816b7e12100577753da3e47443bd7b869c6
DraganovTS <cvetomir_draganov@abv.bg>
2025-11-13 22:17:08 +0200

    Create `user-profile-service` module and enhance `email-service` entity validation
    - Add `user-profile-service` module with necessary configurations in `application.yml` and `pom.xml`.
    - Introduce `EmailUser` entity in `email-service` and enhance `EmailMessage` entity with validation constraints and associations.

--- email-service/src/main/java/org/homemade/email/service/model/entity/EmailMessage.java
+++ email-service/src/main/java/org/homemade/email/service/model/entity/EmailMessage.java
@@ -1,7 +1,11 @@
 package org.homemade.email.service.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.homemade.email.service.model.enums.EmailStatus;
 
 import java.util.Date;
@@ -17,25 +21,37 @@ import java.util.UUID;
 public class EmailMessage {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "email_message_id")
     private UUID id;
-
+    @Email
+    @Column(name = "recipient")
     private String recipient;
-
+    @NotBlank(message = "Subject must not be blank")
+    @Size(max = 100, message = "Subject must be at most 100 characters")
+    @Column(name = "subject", nullable = false)
     private String subject;
-
+    @NotBlank(message = "Body must not be blank")
+    @Size(max = 1000, message = "Body must be at most 1000 characters")
     @Column(columnDefinition = "TEXT")
     private String body;
 
     @Enumerated(EnumType.STRING)
     private EmailStatus status;
-
+    @Column(name = "created_at")
+    @CreationTimestamp
     private Date createdAt;
-
+    @Column(name = "sent_at")
+    @CreationTimestamp
     private Date sentAt;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "email_user_id")
+    private EmailUser emailUser;
 
     @PrePersist
     public void onCreate() {
         createdAt = new Date();
+        sentAt = new Date();
     }
 }
---
+++ email-service/src/main/java/org/homemade/email/service/model/entity/EmailUser.java
@@ -0,0 +1,36 @@
+package org.homemade.email.service.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "email_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailUser {
+    @Id
+    @Column(name = "email_user_id")
+    private UUID emailUserId;
+    @NotBlank(message = "Owner name must not be blank")
+    @Size(max = 30, message = "Owner name must be at most 30 characters")
+    @Column(name = "owner_name")
+    private String ownerName;
+    @Email(message = "Owner email should be valid")
+    @NotBlank(message = "Owner email must not be blank")
+    @Size(max = 30, message = "Owner email must be at most 30 characters")
+    @Column(name = "owner_email")
+    private String ownerEmail;
+    @OneToMany(mappedBy = "email_message_id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EmailMessage> emails;
+}
