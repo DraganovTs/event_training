793b20a97c9df51df9032e331fa97ca77d4efa4d
DraganovTS <cvetomir_draganov@abv.bg>
2025-11-16 19:25:20 +0200

    Add DTOs for `email-service` request and response handling
    - Create `EmailRequestDTO` with validation constraints for recipient, subject, and body fields.
    - Implement `EmailMessageResponseDTO` using Lombok to streamline response structure.

--- email-service/src/main/java/org/homemade/email/service/model/dto/EmailMessageResponseDTO.java
+++ email-service/src/main/java/org/homemade/email/service/model/dto/EmailMessageResponseDTO.java
@@ -1,4 +1,23 @@
 package org.homemade.email.service.model.dto;
 
+
+import lombok.Builder;
+import lombok.Data;
+import org.homemade.email.service.model.enums.EmailStatus;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Builder
 public class EmailMessageResponseDTO {
+
+
+    private UUID id;
+    private String recipient;
+    private String subject;
+    private String body;
+    private EmailStatus status;
+    private Date createdAt;
+    private Date sentAt;
 }
--- email-service/src/main/java/org/homemade/email/service/model/dto/EmailRequestDTO.java
+++ email-service/src/main/java/org/homemade/email/service/model/dto/EmailRequestDTO.java
@@ -1,4 +1,22 @@
 package org.homemade.email.service.model.dto;
 
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
 public class EmailRequestDTO {
+
+    private UUID id;
+    @NotNull(message = "Recipient must not be null")
+    @Size(max = 50, message = "Recipient must be at most 50 characters")
+    private String recipient;
+    @NotNull(message = "Subject must not be null")
+    @Size(max = 50, message = "Subject must be at most 100 characters")
+    private String subject;
+    @NotNull(message = "Body must not be null")
+    @Size(max = 1000, message = "Body must be at most 1000 characters")
+    private String body;
+
 }
