c186e854e5011230fd27b409534aa6345c9def76
DraganovTS <cvetomir_draganov@abv.bg>
2025-11-19 20:26:29 +0200

    Add fields and builder pattern to `EmailsInfoDTO` in `user-profile-service`

--- user-profile-service/src/main/java/org/homemade/user/profile/service/model/dto/EmailsInfoDTO.java
+++ user-profile-service/src/main/java/org/homemade/user/profile/service/model/dto/EmailsInfoDTO.java
@@ -1,4 +1,20 @@
 package org.homemade.user.profile.service.model.dto;
 
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
 public class EmailsInfoDTO {
+
+    private UUID emailId;
+    private String recipient;
+    private String subject;
+    private String body;
+    private String status;
+
 }
