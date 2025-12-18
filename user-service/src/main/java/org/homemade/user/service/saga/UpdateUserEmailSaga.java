0d56ae9a61478fe22ed324903fffbe62a02394af
DraganovTS <cvetomir_draganov@abv.bg>
2025-12-18 20:17:37 +0200

    Enhance `UpdateUserEmailSaga` with command gateway integration, implement rollback mechanism for email updates, and log saga event handling details.

--- user-service/src/main/java/org/homemade/user/service/saga/UpdateUserEmailSaga.java
+++ user-service/src/main/java/org/homemade/user/service/saga/UpdateUserEmailSaga.java
@@ -1,16 +1,53 @@
 package org.homemade.user.service.saga;
 
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.homemade.common.event.choreography.event.UserEmailUpdatedEvent;
+import org.homemade.common.event.orchestration.command.RollbackUserEmailCommand;
+import org.homemade.common.event.orchestration.command.UpdateOwnerEmailCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Nonnull;
 
 @Saga
 @Slf4j
 public class UpdateUserEmailSaga {
 
-    @SagaEventHandler(associationProperty = "userId")
-    public void handle(UserEmailUpdatedEvent event){
+    @Autowired
+    private transient CommandGateway commandGateway;
 
+    @StartSaga
+    @SagaEventHandler(associationProperty = "userId")
+    public void handle(UserEmailUpdatedEvent event) {
+        log.info("Saga Event 1[Start]: Received UserEmailUpdatedEvent for userId: " + event.getUserId());
+        UpdateOwnerEmailCommand command = UpdateOwnerEmailCommand.builder()
+                .ownerId(event.getUserId())
+                .ownerEmail(event.getEmail())
+                .newOwnerEmail(event.getNewEmail())
+                .build();
+
+        commandGateway.send(command, new CommandCallback<>() {
+
+            @Override
+            public void onResult(@Nonnull CommandMessage<? extends UpdateOwnerEmailCommand> commandMessage,
+                                 @Nonnull CommandResultMessage<?> commandResultMessage) {
+                if (commandResultMessage.isExceptional()) {
+                    log.error("Saga Event 1[End]: Failed to update user email for userId: " + event.getUserId());
+                    RollbackUserEmailCommand rollbackUserEmailCommand = RollbackUserEmailCommand.builder()
+                            .userId(event.getUserId())
+                            .email(event.getEmail())
+                            .newEmail(event.getNewEmail())
+                            .errorMessage(commandResultMessage.exceptionResult().getMessage())
+                            .build();
+                    commandGateway.sendAndWait(rollbackUserEmailCommand);
+                }
+            }
+        });
     }
 }
